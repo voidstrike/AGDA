e1af18cd1b273f2c32ac63b2ecfbba6513070aab
alanlin <linyu0077@gmail.com>
2019-03-11 22:35:36 -0500

    Gradually change the code structure; Optimize the performance a little; Add support for SVHN;

--- src/AGDA.py
+++ src/AGDA.py
@@ -1,19 +1,20 @@
 import numpy as np
-import torch
-import sys, os.path
 import os
-import argparse
+import params
+import sys
+import torch
+
+from torchvision.datasets import MNIST, SVHN
+from torch.autograd import Variable
+from torch.utils.data import DataLoader, RandomSampler
+from torchvision import transforms as tfs
+from torch import nn
+from copy import deepcopy
+
 from LinearAE import LinearAE
 from ConvAE import ConvAE, LeNetAE
 from FCNN import LinearClf, Discriminator
-from torchvision.datasets import MNIST, SVHN
-from torch.autograd import Variable
-from torch.utils.data import DataLoader, SubsetRandomSampler
-from torchvision import transforms as tfs
-from torch import nn
-from usps import get_usps
-import params
-from copy import deepcopy
+from usps import USPS
 
 
 def setPartialTrainable(target_model, num_layer):
@@ -69,54 +70,58 @@ def forwardByModelType(in_model, in_vec):
 
 
 def getDataLoader(ds_name, root_path, train=True):
+    # Initially, training data loader & fusion data loader are None
+    target_dl, target_dl_fusion = None, None
+
+    # Get data set by their name
     if ds_name == "mnist":
-        data_set = MNIST(root_path + '/data/mnist', transform=im_tfs, download=True, train=train)
-        target_dl = DataLoader(data_set, batch_size=params.batch_size, shuffle=True)
-        target_dl_fusion = DataLoader(data_set, batch_size=params.fusion_size, shuffle=True)
+        data_set = MNIST(root_path + '/data/mnist',  train=train, transform=im_tfs, download=True)
     elif ds_name == "usps":
-        target_dl = get_usps(root_path + '/data', train)
-        target_dl_fusion = get_usps(root_path + '/data', train, params.fusion_size)
+        data_set = USPS(root_path + '/data', train=train, transform=im_tfs, download=True)
     elif ds_name == "svhn":
-        data_set = SVHN(root_path + '/data/mnist', transform=im_tfs, download=True, split='train' if train else 'test')
-        target_dl = DataLoader(data_set, batch_size=params.batch_size, shuffle=True)
-        target_dl_fusion = DataLoader(data_set, batch_size=params.fusion_size, shuffle=True)
+        data_set = SVHN(root_path + '/data/svhn', split='train' if train else 'test', transform=im_tfs, download=True)
     else:
-        target_dl = None
-        target_dl_fusion = None
+        raise Exception("Unsupported Dataset")
+
+    r_sampler = RandomSampler(data_set, True, params.fusion_size)
+    target_dl = DataLoader(data_set, batch_size=params.batch_size, shuffle=True)
+    if train:
+        target_dl_fusion = DataLoader(data_set, sampler=r_sampler, batch_size=params.fusion_size)
 
     return target_dl, target_dl_fusion
 
 
+# Auxiliary func that convert tensor back to img
 def to_img(x):
     x = 0.5 * (x + 1.)
     x = x.clamp(0, 1)
     x = x.view(x.shape[0], 1, 28, 28)
     return x
 
+
+# Auxiliary torch.transforms object that convert img to tensor
 im_tfs = tfs.Compose([
     tfs.ToTensor()
 ])
 
 
 def main(load_model=False):
-
     root_path = os.getcwd()
 
     # Get source domain data
-    train_set = MNIST(root_path + '/data/mnist', transform=im_tfs, download=True)
-    train_data = DataLoader(train_set, batch_size=params.batch_size, shuffle=True)
-    train_data_fusion = DataLoader(train_set, batch_size=params.fusion_size, shuffle=True)
+    source_train_data, source_train_data_fusion = getDataLoader(params.source_data_set, root_path, True)
 
     # Get target domain data
-    target_train_data = get_usps(root_path + '/data', True)
-    target_test_data = get_usps(root_path + '/data', False)
+    target_train_data, target_train_data_fusion = getDataLoader(params.target_data_set, root_path, True)
+    target_test_data, _ = getDataLoader('usps', root_path, False)
 
-    # Models for source domain
-    # source_ae = BasicAE()
-    # source_ae = LinearAE((28*28, 256, 64, 16, 8), None)  # Generate source AE
-    # source_ae = ConvAE()
+    # Initialize models for source domain
     source_ae = LeNetAE()
     source_clf = LinearClf()
+
+    # Initialize models for target domain
+    # target_ae = LeNetAE()
+    target_dis = Discriminator()
 
     if load_model:
         source_ae.load_state_dict(torch.load(root_path + '/../modeinfo/source_ae.pt'))
@@ -128,7 +133,7 @@ def main(load_model=False):
     criterion_clf = nn.CrossEntropyLoss()           # General Loss of classifier -- CEL
     criterion_gan = nn.BCELoss()                    # Auxiliary loss for GAN (Discriminator)
 
-    sae_opt = torch.optim.Adam(list(source_ae.parameters()) + list(source_clf.parameters()), lr=1e-3)
+    src_optimizer = torch.optim.Adam(list(source_ae.parameters()) + list(source_clf.parameters()), lr=1e-3)
 
     if torch.cuda.is_available():
         source_ae = source_ae.cuda()
@@ -136,14 +141,13 @@ def main(load_model=False):
 
     if not load_model:
         # Train AE & CLF from scratch
+        instance_count = source_train_data.dataset.__len__()
         for step in range(params.clf_train_iter):
-            ae_loss_iter = 0.0
-            clf_loss_iter = 0.0
-            train_acc_iter = 0.0
-            instance_count = 0.0
+            ae_loss_iter, clf_loss_iter, train_acc_iter = .0, .0, .0
 
-            for features, label in train_data:
+            for features, label in source_train_data:
                 instance_count += features.shape[0]
+
                 if torch.cuda.is_available():
                     features = Variable(features.view(features.shape[0], -1).cuda())
                     label = Variable(label.cuda())
@@ -153,25 +157,24 @@ def main(load_model=False):
                     label = Variable(label)
 
                 source_code, source_rec = forwardByModelType(source_ae, features)
-
-                label_pred = source_clf(source_code)
+                label_predict = source_clf(source_code)
 
                 loss_ae = criterion_ae(features, source_rec)
-                loss_clf = criterion_clf(label_pred, label)
+                loss_clf = criterion_clf(label_predict, label)
                 floss = loss_ae + loss_clf
 
-                sae_opt.zero_grad()
+                src_optimizer.zero_grad()
                 floss.backward()
-                sae_opt.step()
+                src_optimizer.step()
 
                 ae_loss_iter += loss_ae.item()
                 clf_loss_iter += loss_clf.item()
 
-                _, pred = label_pred.max(1)
+                _, pred = label_predict.max(1)
                 num_correct = (pred == label).sum().item()
                 train_acc_iter += num_correct
 
-            print('epoch: {}, AutoEncoder Loss: {:.6f}, Classifier Loss: {:.6f}, Train Acc: {:.6f}'
+            print('Epoch: {}, AutoEncoder Loss: {:.6f}, Classifier Loss: {:.6f}, Train Acc: {:.6f}'
                   .format(step, ae_loss_iter / instance_count, clf_loss_iter / instance_count,
                           train_acc_iter / instance_count))
 
@@ -182,7 +185,6 @@ def main(load_model=False):
     target_ae = deepcopy(source_ae)  # Copy from Source AE
     setPartialTrainable(target_ae, params.num_disable_layer)
     # target_ae = LinearAE((28*28, 256, 64, 16, 8), None)
-    target_dis = Discriminator()
 
     optimizer_G = torch.optim.Adam(target_ae.parameters(), lr=1e-4)
     optimizer_D = torch.optim.Adam(target_dis.parameters(), lr=1e-3)
@@ -200,28 +202,16 @@ def main(load_model=False):
 
     # Train target AE and Discriminator
     for step in range(params.tag_train_iter):
-        for features, _ in target_train_data:
-            # Initial Setting for each iteration
-            if features.shape[0] != params.fusion_size:
-                continue  # Drop this block due to size mismatch
-
+        for features, _ in target_train_data_fusion:
             if torch.cuda.is_available():
                 features = Variable(features.view(features.shape[0], -1).cuda())
             else:
                 features = Variable(features.view(features.shape[0], -1))
 
-            # Sample data from source domain
-            t_key = np.random.randint(train_data_fusion.__len__() - 1)
-            sampler = SubsetRandomSampler(list(range(t_key * params.fusion_size, (t_key + 1) * params.fusion_size)))
-            real_loader = DataLoader(train_set, sampler=sampler, shuffle=False, batch_size=params.fusion_size)
-
             real_code = None
-            for s_feature, _ in real_loader:
-                if torch.cuda.is_available():
-                    s_feature = s_feature.cuda()
-
+            for s_feature, _ in source_train_data_fusion:
+                s_feature = s_feature.cuda() if torch.cuda.is_available() else s_feature
                 real_code, _ = forwardByModelType(source_ae, s_feature)
-
             assert real_code is not None
 
             # Train Discriminator k times
@@ -235,7 +225,7 @@ def main(load_model=False):
                 real_loss = criterion_gan(dis_res_real_code, valid_placeholder_fusion)
                 fake_loss = criterion_gan(dis_res_fake_code, fake_placeholder_fusion)
                 d_loss = (real_loss + fake_loss) / 2
-                d_loss.backward()
+                d_loss.backward(retain_graph=True)
 
                 optimizer_D.step()
 
@@ -257,16 +247,12 @@ def main(load_model=False):
         ae_loss_train, train_acc = getModelPerformance(target_train_data, target_ae, source_clf, criterion_ae)
         ae_loss_target, test_acc = getModelPerformance(target_test_data, target_ae, source_clf, criterion_ae)
 
-        print('epoch: {}, AE Loss tra: {:.6f}, Clf Acc Tra: {:.6f}, AE Loss tar: {:.6f}, Clf Acc tar: {:.6f}'
+        print('Epoch: {}, AE Loss train: {:.6f}, Clf Acc Train: {:.6f}, AE Loss Target: {:.6f}, Clf Acc Target: {:.6f}'
               .format(step, ae_loss_train, train_acc, ae_loss_target, test_acc))
 
 
 if __name__ == '__main__':
     Load_flag = False
-    parser = argparse.ArgumentParser(description='Process some integers.')
     if len(sys.argv) > 1:
         Load_flag = bool(sys.argv[1])
     main(Load_flag)
-
-
-
--- src/params.py
+++ src/params.py
@@ -1,7 +1,8 @@
-"""Params for ADDA."""
+"""Params for AGDA."""
 
 # params for dataset and data loader
 data_root = "data"
+# TODO
 # batch_size must equal fusion_size for early fusion model
 batch_size = 128
 fusion_size = 128
@@ -11,6 +12,10 @@ image_size = 64
 tgt_dataset = "USPS"
 tgt_encoder_restore = "snapshots/ADDA-target-encoder-final.pt"
 tgt_model_trained = True
+
+# params for data set selection
+source_data_set = "mnist"
+target_data_set = "usps"
 
 # params for model training
 clf_train_iter = 200
@@ -23,5 +28,4 @@ d_steps = 1
 
 num_disable_layer = 0
 
-source_data_set = "MNIST"
-target_data_set = "USPS"
+
