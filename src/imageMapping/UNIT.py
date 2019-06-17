b38a93c8d2a9a387ad1a9f1d7eecc539188fff8b
alanlin <linyu0077@gmail.com>
2019-06-17 16:06:00 -0500

    Add code for UNIT Algorithm

---
+++ src/imageMapping/UNIT.py
@@ -0,0 +1,234 @@
+# Latent Code Sharing Assumption
+
+from model.ResNet_UNIT import *
+
+import math
+import os
+import itertools
+import torchvision.transforms as transforms
+import datetime
+import time
+import sys
+from torchvision.utils import save_image
+
+from torch.utils.data import DataLoader
+from torchvision.datasets import ImageFolder
+from torchvision import datasets
+from torch.autograd import Variable
+
+from torch.optim import lr_scheduler
+
+
+def get_all_data_loader(root_path, transformer, batch_size=32):
+    root_path = os.path.join(root_path, 'data/unit')
+
+    train_x = get_data_loader_folder(os.path.join(root_path, 'trainX'), transformer, batch_size)
+    test_x = get_data_loader_folder(os.path.join(root_path, 'testX'), transformer, batch_size, shuffle=False)
+
+    train_y = get_data_loader_folder(os.path.join(root_path, 'trainY'), transformer, batch_size)
+    test_y = get_data_loader_folder(os.path.join(root_path, 'testY'), transformer, batch_size, shuffle=False)
+
+    return train_x, train_y, test_x, test_y
+
+
+def get_data_loader_folder(input_folder, transformer, batch_size, shuffle=True):
+    dataset = ImageFolder(input_folder, transform=transformer)
+    loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=shuffle, drop_last=True)
+    return loader
+
+
+def compute_kl(mu):
+    mu_2 = torch.pow(mu, 2)
+    loss = torch.mean(mu_2)
+    return loss
+
+
+def main():
+    # Specify input shape
+    input_shape = (3, 256, 256)
+    dim = 64  # Number of filters in the first Conv layer
+    learning_rate = 1e-4
+    n_downsample = 2
+    shared_dim = dim * 2 ** n_downsample
+    n_epochs = 200
+    epoch = 0
+    decay_epoch = 100
+    global_batch_size = 1
+    # Initialize generator and discriminator
+
+    shared_E = ResidualBlock(features=shared_dim)
+    E1 = Encoder(dim=dim, n_downsample=n_downsample, shared_block=shared_E)
+    E2 = Encoder(dim=dim, n_downsample=n_downsample, shared_block=shared_E)
+    shared_G = ResidualBlock(features=shared_dim)
+
+    G1 = Decoder(dim=dim, n_upsample=n_downsample, shared_block=shared_G)
+    G2 = Decoder(dim=dim, n_upsample=n_downsample, shared_block=shared_G)
+
+    D1 = Discriminator(input_shape)
+    D2 = Discriminator(input_shape)
+
+    criterion_GAN = torch.nn.MSELoss()
+    criterion_pixel = torch.nn.L1Loss()
+
+    if torch.cuda.is_available():
+        E1 = E1.cuda()
+        E2 = E2.cuda()
+        G1 = G1.cuda()
+        G2 = G2.cuda()
+        D1 = D1.cuda()
+        D2 = D2.cuda()
+        criterion_GAN.cuda()
+        criterion_pixel.cuda()
+
+    # Loss weights
+    lambda_0 = 10  # GAN
+    lambda_1 = 0.1  # KL (encoded images)
+    lambda_2 = 100  # ID pixel-wise
+    lambda_3 = 0.1  # KL (encoded translated images)
+    lambda_4 = 100  # Cycle pixel-wise
+
+    # Optimizers
+    optimizer_G = torch.optim.Adam(
+        itertools.chain(E1.parameters(), E2.parameters(), G1.parameters(), G2.parameters()),
+        lr=learning_rate,
+        betas=(.5, .999),
+    )
+    optimizer_D1 = torch.optim.Adam(D1.parameters(), lr=learning_rate, betas=(.5, .999))
+    optimizer_D2 = torch.optim.Adam(D2.parameters(), lr=learning_rate, betas=(.5, .999))
+
+    # Learning rate update schedulers -- Currently set to constant
+    lr_scheduler_G = None
+    lr_scheduler_D1 = None
+    lr_scheduler_D2 = None
+
+    Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.Tensor
+
+    # Temporary Image Transformation
+    transforms_ = [
+        transforms.Resize(int(input_shape[1] * 1.12)),
+        transforms.RandomCrop((input_shape[1], input_shape[2])),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    ]
+
+    traX, traY, tesX, tesY = get_all_data_loader(os.getcwd(), transforms, 32)
+
+    prev_time = time.time()
+
+    # -------------------------------
+    #  Train Encoders and Generators (Decoders)
+    # -------------------------------
+
+    for current_epoch in range(n_epochs):
+        for pivot, (train_img_X, train_img_Y) in enumerate(zip(traX, traY)):
+            if torch.cuda.is_available():
+                X1 = Variable(train_img_X.type(Tensor).cuda())
+                X2 = Variable(train_img_Y.type(Tensor).cuda())
+                valid = Variable(Tensor(np.ones((X1.size(0), *D1.output_shape))).cuda(), requires_grad=False)
+                fake = Variable(Tensor(np.zeros((X1.size(0), *D1.output_shape))).cuda(), requires_grad=False)
+            else:
+                X1 = Variable(train_img_X.type(Tensor))
+                X2 = Variable(train_img_Y.type(Tensor))
+                valid = Variable(Tensor(np.ones((X1.size(0), *D1.output_shape))), requires_grad=False)
+                fake = Variable(Tensor(np.zeros((X1.size(0), *D1.output_shape))), requires_grad=False)
+
+
+            optimizer_G.zero_grad()
+
+            # Get shared latent representation
+            mu1, Z1 = E1(X1)
+            mu2, Z2 = E2(X2)
+
+            # Reconstruct images
+            rec_X1 = G1(Z1)
+            rec_X2 = G2(Z2)
+
+            # Translate images
+            fake_X1 = G1(Z2)
+            fake_X2 = G2(Z1)
+
+            # Cycle translation
+            mu1_, Z1_ = E1(fake_X1)
+            mu2_, Z2_ = E2(fake_X2)
+            cyc_X1 = G1(Z2_)
+            cyc_X2 = G2(Z1_)
+
+            # Losses
+            loss_GAN_1 = lambda_0 * criterion_GAN(D1(fake_X1), valid)
+            loss_GAN_2 = lambda_0 * criterion_GAN(D2(fake_X2), valid)
+            loss_KL_1 = lambda_1 * compute_kl(mu1)
+            loss_KL_2 = lambda_1 * compute_kl(mu2)
+            loss_ID_1 = lambda_2 * criterion_pixel(rec_X1, X1)
+            loss_ID_2 = lambda_2 * criterion_pixel(rec_X2, X2)
+            loss_KL_1_ = lambda_3 * compute_kl(mu1_)
+            loss_KL_2_ = lambda_3 * compute_kl(mu2_)
+            loss_cyc_1 = lambda_4 * criterion_pixel(cyc_X1, X1)
+            loss_cyc_2 = lambda_4 * criterion_pixel(cyc_X2, X2)
+
+            # Total loss
+            loss_G = (
+                    loss_KL_1
+                    + loss_KL_2
+                    + loss_ID_1
+                    + loss_ID_2
+                    + loss_GAN_1
+                    + loss_GAN_2
+                    + loss_KL_1_
+                    + loss_KL_2_
+                    + loss_cyc_1
+                    + loss_cyc_2
+            )
+
+            loss_G.backward()
+            optimizer_G.step()
+
+            # -----------------------
+            #  Train Discriminator 1
+            # -----------------------
+
+            optimizer_D1.zero_grad()
+
+            loss_D1 = criterion_GAN(D1(X1), valid) + criterion_GAN(D1(fake_X1.detach()), fake)
+
+            loss_D1.backward()
+            optimizer_D1.step()
+
+            # -----------------------
+            #  Train Discriminator 2
+            # -----------------------
+
+            optimizer_D2.zero_grad()
+
+            loss_D2 = criterion_GAN(D2(X2), valid) + criterion_GAN(D2(fake_X2.detach()), fake)
+
+            loss_D2.backward()
+            optimizer_D2.step()
+
+            # --------------
+            #  Log Progress
+            # --------------
+
+            # Determine approximate time left
+            batches_done = current_epoch * len(traX) + pivot * global_batch_size
+            batches_left = n_epochs * len(traX) - batches_done
+            time_left = datetime.timedelta(seconds=batches_left * (time.time() - prev_time))
+            prev_time = time.time()
+
+            # Print log
+            sys.stdout.write(
+                "\r[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f] ETA: %s"
+                % (epoch, n_epochs, pivot, len(traX), (loss_D1 + loss_D2).item(), loss_G.item(), time_left)
+            )
+
+            # Update learning rates
+        if lr_scheduler_G is not None:
+            lr_scheduler_G.step()
+        if lr_scheduler_D1 is not None:
+            lr_scheduler_D1.step()
+        if lr_scheduler_D2 is not None:
+            lr_scheduler_D2.step()
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/model/ResNet_UNIT.py
@@ -0,0 +1,130 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+from torch.autograd import Variable
+import numpy as np
+
+class ResidualBlock(nn.Module):
+    def __init__(self, features):
+        super(ResidualBlock, self).__init__()
+
+        conv_block = [
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(features, features, 3),
+            nn.InstanceNorm2d(features),
+            nn.ReLU(inplace=True),
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(features, features, 3),
+            nn.InstanceNorm2d(features),
+        ]
+
+        self.conv_block = nn.Sequential(*conv_block)
+
+    def forward(self, x):
+        return x + self.conv_block(x)
+
+
+class Encoder(nn.Module):
+    def __init__(self, in_channels=3, dim=64, n_downsample=2, shared_block=None):
+        super(Encoder, self).__init__()
+
+        # Initial convolution block
+        layers = [
+            nn.ReflectionPad2d(3),
+            nn.Conv2d(in_channels, dim, 7),
+            nn.InstanceNorm2d(dim),
+            nn.LeakyReLU(0.2, inplace=True),
+        ]
+
+        # Downsampling
+        for _ in range(n_downsample):
+            layers += [
+                nn.Conv2d(dim, dim * 2, 4, stride=2, padding=1),
+                nn.InstanceNorm2d(dim * 2),
+                nn.ReLU(inplace=True),
+            ]
+            dim *= 2
+
+        # Residual blocks
+        for _ in range(3):
+            layers += [ResidualBlock(dim)]
+
+        self.model_blocks = nn.Sequential(*layers)
+        self.shared_block = shared_block
+
+    def reparameterization(self, mu):
+        # Reduced VAE -> The outputs are multivariate Gaussian distribution with mean = mu & std = 1
+        Tensor = torch.cuda.FloatTensor if mu.is_cuda else torch.FloatTensor
+        z = Variable(Tensor(np.random.normal(0, 1, mu.shape)))
+        return z + mu
+
+    def forward(self, x):
+        x = self.model_blocks(x)
+        mu = self.shared_block(x)
+        z = self.reparameterization(mu)
+        return mu, z
+
+
+class Decoder(nn.Module):
+    def __init__(self, out_channels=3, dim=64, n_upsample=2, shared_block=None):
+        super(Decoder, self).__init__()
+
+        self.shared_block = shared_block
+
+        layers = []
+        dim = dim * 2 ** n_upsample
+        # Residual blocks
+        for _ in range(3):
+            layers += [ResidualBlock(dim)]
+
+        # Upsampling
+        for _ in range(n_upsample):
+            layers += [
+                nn.ConvTranspose2d(dim, dim // 2, 4, stride=2, padding=1),
+                nn.InstanceNorm2d(dim // 2),
+                nn.LeakyReLU(0.2, inplace=True),
+            ]
+            dim = dim // 2
+
+        # Output layer
+        layers += [nn.ReflectionPad2d(3), nn.Conv2d(dim, out_channels, 7), nn.Tanh()]
+
+        self.model_blocks = nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.shared_block(x)
+        x = self.model_blocks(x)
+        return x
+
+
+class DisBlock(nn.Module):
+    def __init__(self, in_filters, out_filters, normalize=True):
+        super(DisBlock, self).__init__()
+        # Basic Discriminator Block -> kernel_size=4 & stride=2 => reduce the height & width by half
+        layers = [nn.Conv2d(in_filters, out_filters, 4, stride=2, padding=1)]
+        if normalize:
+            layers.append(nn.InstanceNorm2d(out_filters))
+        layers.append(nn.LeakyReLU(.2, inplace=True))
+        self.layers = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.layers(x)
+
+
+class Discriminator(nn.Module):
+    def __init__(self, input_shape):
+        super(Discriminator, self).__init__()
+        channels, height, width = input_shape
+        # Calculate output of image discriminator (PatchGAN)
+        self.output_shape = (1, height // 2 ** 4, width // 2 ** 4)
+
+        self.model = nn.Sequential(
+            DisBlock(channels, 64, normalize=False),
+            DisBlock(64, 128),
+            DisBlock(128, 256),
+            DisBlock(256, 512),
+            nn.Conv2d(512, 1, 3, padding=1)
+        )
+
+    def forward(self, img):
+        return self.model(img)
